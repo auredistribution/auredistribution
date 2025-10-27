68a2c148aa5bc0a29762310b1a280a0319e61a83
Jaxen Wood <jwood74@gmail.com>
2025-10-27 21:23:08 +1000

    Additional Instructions

--- js/snippets.js
+++ js/snippets.js
@@ -5,17 +5,21 @@ window.REDIST_SHARED_SNIPPETS = {
   headings: {
     instructions: 'Instructions',
     disclaimer: 'Disclaimer',
-    attribution: 'Attribution'
+    attribution: 'Contributions'
   },
   instructions: [
-    '1. Click any district in the left panel (within the Divisions tab) to select or unselect it',
-    '2. Click on any SA1 on the map to transfer it into the selected district',
-    '3. Click on any transferred SA1s to return them to their original district',
-    '4. Shift-click can be used to transfer or return whole SA2s',
-    "5. New divisions can be created by clicking the 'New' button next to a group label",
+    '1. Click the Divisions tab up top to view the list of divisions, and their current enrolments',
+    '2. Click one of the divisions to select or unselect it',
+    '3. Click on any SA1 on the map to transfer it into the selected district',
+    '4. Click on any transferred SA1s to return them to their original district',
+    '5. Shift-click an SA1 to transfer or return the whole SA2 at once',
+    '6. Right-click an SA1 on the map to select the SA1\'s division without transferring it',
+    "7. New divisions can be created by clicking the 'New' button next to a group label",
+    "8. Clicking a Group Label will collapse that group",
+    "9. Right-click a division to edit its name or alter its colour"
   ],
   disclaimer: 'This tool presents raw data provided by official sources. Calculations use ' +
     'published enrolment data; aggregated results are provided without warranty. No guarantee is ' +
     'made that aggregate enrolment calculations will be error-free. The tool is offered as-is, and any usage of the tool is made at the user\'s own risk.',
-  attribution: 'The original idea and project was developed by Angas, available <a href="https://auredistribution.neocities.org/" target="_blank" rel="noopener">here</a> and <a href="https://github.com/auredistribution/qld-state-redistribution" target="_blank" rel="noopener">on GitHub</a>.'
+  attribution: 'Ideas and suggestions welcome - submit an Issue on <a target="_blank" href="https://github.com/auredistribution/auredistribution">GitHub</a>.'
 };
